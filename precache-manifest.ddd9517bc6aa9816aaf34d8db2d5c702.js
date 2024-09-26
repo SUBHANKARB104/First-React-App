2c02b365138654ce13c008ff6c712cde850b684f
SUBHANKARB104 <b104.subhankar@gmail.com>
2024-09-27 02:47:02 +0530

    Updates

---
+++ precache-manifest.ddd9517bc6aa9816aaf34d8db2d5c702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6d6d7df316e77f546deddc92aaede2",
+    "url": "/First-React-App/index.html"
+  },
+  {
+    "revision": "c0de2bccb95ea67c175d",
+    "url": "/First-React-App/static/css/main.7fd9ba4b.chunk.css"
+  },
+  {
+    "revision": "92e23421ff612ea251ce",
+    "url": "/First-React-App/static/js/2.8851f76e.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/First-React-App/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "c0de2bccb95ea67c175d",
+    "url": "/First-React-App/static/js/main.3626eb76.chunk.js"
+  },
+  {
+    "revision": "bf115483909fdd07107a",
+    "url": "/First-React-App/static/js/runtime~main.6e62791c.js"
+  }
+]);
